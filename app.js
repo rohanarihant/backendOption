62dbc3fb776bfb2729ae7376075d31c65c429f84
Rohan <rohan.arihant@gmail.com>
2021-08-28 21:56:40 +0530

    timezone ist

--- app.js
+++ app.js
@@ -136,6 +136,6 @@ setInterval(async() => {
     }catch(err){
         console.log(err,'errrior')
     }
-},6000);
+},120000);
 
 app.listen(process.env.PORT || 5000, () => console.log(`Example app listening on port ${port}!`))
